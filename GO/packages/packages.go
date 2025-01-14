276f696b9e4f45b4a1c4e61479e936a5c0970206
MakoskiVictor <makoski.ed@gmail.com>
2025-01-14 10:11:29 -0300

    math.Sqrt

--- GO/packages/packages.go
+++ GO/packages/packages.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Println("My favourite number is ", rand.Intn(10))
+	fmt.Printf("We have %g problems\n", math.Sqrt(25))
 }
