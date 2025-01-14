8f0b66752f93058147e93d601c3792a5e6c36a2e
MakoskiVictor <makoski.ed@gmail.com>
2025-01-14 11:09:36 -0300

    variables

---
+++ GO/variables/variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var c, python, java bool = true, true, false
+
+func main() {
+	var i int
+	var str string = "Hello my little fellow\n"
+	fmt.Println(i, str, c, python, java)
+}
