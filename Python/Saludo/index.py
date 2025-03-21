262fcced87b0df32e14e3836f3ea8763a7f7d257
MakoskiVictor <makoski.ed@gmail.com>
2025-03-21 16:39:12 -0300

    feat: add try/except

--- Python/Saludo/index.py
+++ Python/Saludo/index.py
@@ -1,13 +1,31 @@
+from rich.console import Console
+
+# Inicializo la consola de rich
+console = Console()
 # Pedir nombre
 nombre = input('Hola, usuario, ¿cómo te llamas? \nNombre: ')
 
 # Pedir edad
 edad = input(f'¡Hola, {nombre}! ¿Cuántos años tienes? \nEdad: ')
 
-# Convertir edad en un número
-edad = int(edad)
+# Manejador de errores por si no ingresa un número
+try:
+    # Convertir edad en un número
+    edad = int(edad)
+    if edad >= 18:
+        print(f'Vaya, {nombre}, veo que eres mayor de edad.\n')
+    else:
+        print(f'Vaya, {nombre}, veo que eres menor de edad.\n')
+except ValueError:
+    console.print('Debías ingresar un número válido :(')
+    exit()
 
-if edad >= 18:
-    print(f'Vaya, {nombre}, veo que eres mayor de edad.')
+
+continuar = console.input(prompt='¿Te gustaría seguir aprendiendo? (Sí/No) \n>')
+
+if continuar.lower() in ('sí', 'si', 's'):
+    console.print('¡Perfecto, continuemos en la siguiente App!')
+elif continuar.lower() in ('no', 'n'):
+    console.print('Una lástima. Nos vemos :(')
 else:
-    print(f'Vaya, {nombre}, veo que eres menor de edad.')
+    console.print('Lo lamento, no entiendo tu respuesta. Por favor, solo responde con Sí o No')
