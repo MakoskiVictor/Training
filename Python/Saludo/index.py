7aa04349b7473c3399609e2c7cecc3337fe74816
MakoskiVictor <makoski.ed@gmail.com>
2025-03-21 16:49:29 -0300

    Feat: add styles

--- Python/Saludo/index.py
+++ Python/Saludo/index.py
@@ -3,29 +3,29 @@ from rich.console import Console
 # Inicializo la consola de rich
 console = Console()
 # Pedir nombre
-nombre = input('Hola, usuario, ¿cómo te llamas? \nNombre: ')
+nombre = console.input('[bold green]Hola, usuario, ¿cómo te llamas? \n[bold yellow]Nombre: ')
 
 # Pedir edad
-edad = input(f'¡Hola, {nombre}! ¿Cuántos años tienes? \nEdad: ')
+edad = console.input(f'[bold green]¡Hola, {nombre}! ¿Cuántos años tienes? \n[bold yellow]Edad: ')
 
 # Manejador de errores por si no ingresa un número
 try:
     # Convertir edad en un número
     edad = int(edad)
     if edad >= 18:
-        print(f'Vaya, {nombre}, veo que eres mayor de edad.\n')
+        console.print(f'[bold green]Vaya, {nombre}, veo que eres mayor de edad.\n')
     else:
-        print(f'Vaya, {nombre}, veo que eres menor de edad.\n')
+        console.print(f'[bold green]Vaya, {nombre}, veo que eres menor de edad.\n')
 except ValueError:
-    console.print('Debías ingresar un número válido :(')
+    console.print('[bold red]Debías ingresar un número válido :(')
     exit()
 
 
-continuar = console.input(prompt='¿Te gustaría seguir aprendiendo? (Sí/No) \n>')
+continuar = console.input(prompt='¿Te gustaría seguir aprendiendo? (Sí/No) \n[bold yellow]>')
 
 if continuar.lower() in ('sí', 'si', 's'):
-    console.print('¡Perfecto, continuemos en la siguiente App!')
+    console.print('[bold green]¡Perfecto, continuemos en la siguiente App!')
 elif continuar.lower() in ('no', 'n'):
-    console.print('Una lástima. Nos vemos :(')
+    console.print('[bold red]Una lástima. Nos vemos :(')
 else:
-    console.print('Lo lamento, no entiendo tu respuesta. Por favor, solo responde con Sí o No')
+    console.print('[bold red]Lo lamento, no entiendo tu respuesta. Por favor, solo responde con Sí o No')
